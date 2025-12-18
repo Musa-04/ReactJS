4aed3c7511bf642e3f2fcb095fb48a60676f5b35
Musa-04 <mustafeezshaikh88@gmail.com>
2025-12-19 01:07:41 +0530

    react practice

---
+++ 03_props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/card.jsx'
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card/>
+      <Card/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 03_props/src/components/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div className='card'>
+              <img src="https://plus.unsplash.com/premium_vector-1712873279566-379ba42df159?w=352&dpr=2&h=367&auto=format&fit=crop&q=60&ixlib=rb-4.1.0" alt="" />
+              <h1>Mustafeez</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <button>View Profile</button>
+            </div>
+    </div>
+  )
+}
+
+export default card
