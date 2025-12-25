b85c1e8b38c6741703bf3105b20fc1b3805ea9e5
Musa-04 <mustafeezshaikh88@gmail.com>
2025-12-25 20:16:35 +0530

    Added 08_functions project

---
+++ 08_functions/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const App = () => {
+
+
+const pageScrolling=(elem)=>{
+  // console.log('page scrolling.... at speed ' + elem);
+  if(elem>0){
+    console.log('seedha Scrolling');
+    
+  }
+  else{
+    console.log('ulta Scrolling');
+    
+  }
+  
+}
+
+  return (
+    <div onWheel={(elem)=>{
+    pageScrolling(elem.deltaY)
+      
+
+    }}>
+     <div className='page1'></div>
+     <div className='page2'></div>
+     <div className='page3'></div>
+  </div>
+  )
+}
+
+export default App
